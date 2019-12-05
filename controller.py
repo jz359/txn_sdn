f5fec32e4d6102b28e143ab92e62730a1e2c7213
Andy Zhang <jz359@cornell.edu>
2019-12-05 17:29:26 -0500

    fixed synchronization issues; isolation now works but for semi-wrong reasons

--- controller.py
+++ controller.py
@@ -20,14 +20,6 @@ import p4runtime_lib.bmv2
 from p4runtime_lib.switch import ShutdownAllSwitchConnections
 import p4runtime_lib.helper
 
-switches = {}
-p4info_helper = None
-PARTICIPANTS = 1
-
-response_list = {}
-access_lock = threading.Lock()
-got_all_responses = threading.Condition(access_lock)
-
 
 class Vote(Packet):
     name = "vote"
@@ -92,6 +84,18 @@ def commit_pkt(txn_id, txn_mgr, iface):
     pkt = pkt /TwoPCPhase(phase=3) / Commit(txn_mgr=txn_mgr, txn_id=txn_id)
     return pkt
 
+
+def get_packet_layer(packet, desired_layer):
+    counter = 0
+    while True:
+        layer = packet.getlayer(counter)
+        if layer is None:
+            break
+        if layer.name == desired_layer:
+            return layer
+        counter += 1
+
+
 def get_if(sw):
     ifs=get_if_list()
     target = 'ctlr-' + sw
@@ -112,10 +116,12 @@ def print_pkt(pkt):
 
 
 class Sniffer(threading.Thread):
-    def __init__(self, iface, queue):
+    def __init__(self, txn_mgr, iface, queue, phase):
         super(Sniffer, self).__init__()
+        self.txn_mgr = txn_mgr
         self.iface = iface
         self.queue = queue
+        self.phase = phase
 
 
     def run(self):
@@ -125,45 +131,50 @@ class Sniffer(threading.Thread):
 
 
     def add_pkt(self, pkt):
-        if (pkt.src == 'ff:ff:ff:ff:ff:ff'):
+        response_layer = None
+        if self.phase == 'vote':
+            response_layer = 'confirm'
+        elif self.phase == 'release':
+            response_layer = 'free'
+        elif self.phase == 'commit':
+            response_layer = 'finished'
+
+        layer = get_packet_layer(pkt, response_layer)
+        if (layer is not None and (pkt.src == 'ff:ff:ff:ff:ff:ff') and (layer.txn_mgr == self.txn_mgr)):
             self.queue.put(pkt)
             sys.exit(0)
 
 class Runner(threading.Thread):
-    def __init__(self, txn_mgr, txn_id, phase, sw):
+    def __init__(self, txn_mgr, txn_id, phase, sw, response_list, access_lock, got_all_responses, participants):
         super(Runner, self).__init__()
         self.txn_mgr = txn_mgr
         self.txn_id = txn_id
         self.phase = phase
         self.sw = sw
         self.queue = Queue.Queue()
-        self.sniffer = Sniffer(iface="ctlr-"+self.sw, queue=self.queue)
+
+        # synchronization variables
+        self.response_list = response_list
+        self.access_lock = access_lock
+        self.got_all_responses = got_all_responses
+        self.participants = participants
+
+        self.sniffer = Sniffer(txn_mgr=self.txn_mgr, iface="ctlr-"+self.sw, queue=self.queue, phase=self.phase)
         self.sniffer.start()
 
-
-    def get_packet_layer(self, packet, desired_layer):
-        counter = 0
-        while True:
-            layer = packet.getlayer(counter)
-            if layer is None:
-                break
-            if layer.name == desired_layer:
-                return layer
-            counter += 1
 
     def run_vote(self):
         iface = get_if(self.sw)
         pkt = vote_pkt(self.txn_id, self.txn_mgr, iface)
-        print('running vote')
-        print(self.txn_mgr,self.txn_id)
-        print_pkt(pkt)
+        print('CONTROLLER ' + str(self.txn_mgr) + ': send vote to ' + self.sw)
         sendp(pkt, iface=iface, verbose=False)
         try:
             resp_pkt = self.queue.get(timeout=5)
         except:
+            print('CONTROLLER ' + str(self.txn_mgr) + ': timeout on vote-reply from ' + self.sw)
             return 1 # failure
-        print('CONTROLLER ' + str(self.txn_mgr) + ': got a vote from ' + self.sw)
-        layer = self.get_packet_layer(resp_pkt, 'confirm')
+        print('CONTROLLER ' + str(self.txn_mgr) + ': got vote-reply from ' + self.sw)
+        layer = get_packet_layer(resp_pkt, 'confirm')
         if layer is None:
             return 1 # failure
         return layer.status
@@ -171,33 +182,36 @@ class Runner(threading.Thread):
     def run_release(self):
         iface = get_if(self.sw)
         pkt = release_pkt(self.txn_id, self.txn_mgr, iface)
-        print('CONTROLLER ' + str(self.txn_mgr) + ': running release')
+        print('CONTROLLER ' + str(self.txn_mgr) + ': send release to ' + self.sw)
         sendp(pkt, iface=iface, verbose=False)
         try:
             resp_pkt = self.queue.get(timeout=5)
         except:
+            print('CONTROLLER ' + str(self.txn_mgr) + ': timeout on release-reply from ' + self.sw)
             return 1 # failure
-        layer = self.get_packet_layer(resp_pkt, 'free')
+        print('CONTROLLER ' + str(self.txn_mgr) + ': got release-reply from ' + self.sw)
+        layer = get_packet_layer(resp_pkt, 'free')
         return 0 # success
 
 
     def run_commit(self):
         iface = get_if(self.sw)
         pkt = commit_pkt(self.txn_id, self.txn_mgr, iface)
-        print('running commit')
+        print('CONTROLLER ' + str(self.txn_mgr) + ': send commit to ' + self.sw)
         sendp(pkt, iface=iface, verbose=False)
         try:
             resp_pkt = self.queue.get(timeout=5)
         except:
+            print('CONTROLLER ' + str(self.txn_mgr) + ': timeout on commit-reply from ' + self.sw)
             return 1 # failure
         # print_pkt(resp_pkt)
-        print('CONTROLLER ' + str(self.txn_mgr) + ': got commit ok from ' + self.sw)
-        layer = self.get_packet_layer(resp_pkt, 'finished')
+        print('CONTROLLER ' + str(self.txn_mgr) + ': got commit-reply from ' + self.sw)
+        layer = get_packet_layer(resp_pkt, 'finished')
         return 0 # success
 
 
     def run(self):
-        global response_list, got_all_responses, access_lock, PARTICIPANTS
+        # global response_list, got_all_responses, access_lock, PARTICIPANTS
         if self.phase == "vote":
             response = self.run_vote()
         elif self.phase == "release":
@@ -207,10 +221,10 @@ class Runner(threading.Thread):
         else:
             print("wtf")
 
-        with access_lock:
-            response_list[self.sw] = response
-            if len(response_list.keys()) == PARTICIPANTS:
-                got_all_responses.notifyAll()
+        with self.access_lock:
+            self.response_list[self.sw] = response
+            if len(self.response_list.keys()) == self.participants:
+                self.got_all_responses.notifyAll()
 
         # done running a phase
         sys.exit(0)
@@ -227,45 +241,51 @@ class TransactionManager(object):
         self.main_q = main_q
         self.main_q_ack = main_q_ack
 
+        # synchronization variables
+        self.response_list = {}
+        self.access_lock = threading.Lock()
+        self.got_all_responses = threading.Condition(self.access_lock)
+        self.PARTICIPANTS = 0
+
 
     def set_participants(self, updates):
         for _, update in updates.items():
             self.participants.add(str(update['SWITCH']))
 
     def run_txn(self, txn_id, updates):
-        global response_list, got_all_responses, access_lock, PARTICIPANTS
-        response_list = {}
+        # global response_list, got_all_responses, access_lock, PARTICIPANTS
+        self.response_list = {}
         self.updates[txn_id] = updates
         self.set_participants(updates)
-        PARTICIPANTS = len(self.participants)
+        self.PARTICIPANTS = len(self.participants)
 
         for sw in self.participants:
-            r = Runner(self.txn_mgr,txn_id,"vote", sw)
+            r = Runner(self.txn_mgr,txn_id,"vote", sw, response_list=self.response_list, access_lock=self.access_lock, got_all_responses=self.got_all_responses, participants=self.PARTICIPANTS)
             r.start()
 
-        with access_lock:
-            while (len(response_list.keys()) < PARTICIPANTS):
-                got_all_responses.wait()
+        with self.access_lock:
+            while (len(self.response_list.keys()) < self.PARTICIPANTS):
+                self.got_all_responses.wait()
 
         num_nacks = 0
         ack_switches = set()
-        for sw, response in response_list.items():
+        for sw, response in self.response_list.items():
             if response:
                 num_nacks += 1
             else:
                 ack_switches.add(sw)
 
         # clear the response list
-        response_list = {}
+        self.response_list = {}
 
         if num_nacks > 0:
             print('CONTROLLER ' + str(self.txn_mgr) + ': cannot acquire all locks; proceeding to release phase')
             for sw in self.participants:
-                r = Runner(self.txn_mgr,txn_id,"release", sw)
+                r = Runner(self.txn_mgr,txn_id,"release", sw, response_list=self.response_list, access_lock=self.access_lock, got_all_responses=self.got_all_responses, participants=self.PARTICIPANTS)
                 r.start()
-            with access_lock:
-                while (len(response_list.keys()) < PARTICIPANTS):
-                    got_all_responses.wait()
+            with self.access_lock:
+                while (len(self.response_list.keys()) < self.PARTICIPANTS):
+                    self.got_all_responses.wait()
             return
 
         # else, got all acks so proceed to commit phase
@@ -273,12 +293,12 @@ class TransactionManager(object):
         self.main_q_ack.get()
 
         for sw in self.participants:
-            r = Runner(self.txn_mgr,txn_id,"commit", sw)
+            r = Runner(self.txn_mgr,txn_id,"commit", sw, response_list=self.response_list, access_lock=self.access_lock, got_all_responses=self.got_all_responses, participants=self.PARTICIPANTS)
             r.start()
 
-        with access_lock:
-            while (len(response_list.keys()) < PARTICIPANTS):
-                got_all_responses.wait()
+        with self.access_lock:
+            while (len(self.response_list.keys()) < self.PARTICIPANTS):
+                self.got_all_responses.wait()
         print('CONTROLLER ' + str(self.txn_mgr) + ': commit phase done and all locks were released!')
 
 
